a569b82145cb85ff8ce87cbad3abee75e1a9d5d5
ztt0tian <1290507445@qq.com>
2017-09-07 12:58:48 +0800

    Initial commit using new project wizard

---
+++ app/src/main/java/com/example/asus_wh/reminders/RemindersActiviy.java
@@ -0,0 +1,13 @@
+package com.example.asus_wh.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemindersActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders_activiy);
+    }
+}
