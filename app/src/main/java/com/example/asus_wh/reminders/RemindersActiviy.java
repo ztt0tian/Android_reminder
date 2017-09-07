18ccfda6f26f4ef6c01b7a2073085f608876007d
ztt0tian <1290507445@qq.com>
2017-09-07 15:03:00 +0800

    Adds new reminder and exit menu options

--- app/src/main/java/com/example/asus_wh/reminders/RemindersActiviy.java
+++ app/src/main/java/com/example/asus_wh/reminders/RemindersActiviy.java
@@ -1,7 +1,9 @@
 package com.example.asus_wh.reminders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,12 +13,25 @@ public class RemindersActiviy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders_activiy);
-        mListView=(ListView) findViewById(R.id.reminders_list_view);
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(
-                this,R.layout.reminders_row,R.id.row_text,new String[]{
-                "first record","second record","third record"
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.reminders_row, R.id.row_text, new String[]{
+                "first record", "second record", "third record"
         }
         );
         mListView.setAdapter(arrayAdapter);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_new:
+                Log.d(getLocalClassName(),"create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return  true;
+            default:
+                return false;
+        }
     }
 }
